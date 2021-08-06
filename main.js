20181f95509bfc7440334bf9febf4dd00d2c9b77
Hugo Perez <hugopereztm12309@gmail.com>
2021-08-06 16:40:33 -0400

    Just chnaged a function parameter for testing, and mulitplied the size by itself for appropriate proportions.

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-
-
-
-
-
 const div = document.querySelector('#grid');
 
+createGridBoxes(4);
+
 function createGridBoxes(size) {
-    for (let i = 0; i < size; i++) {
+    for (let i = 0; i < size * size; i++) {
         let gridBox = document.createElement('div');
-        gridBox.classList.add('gridBox');
-        div.appendChild(gridBox);
+            gridBox.classList.add('gridBox');
+                div.appendChild(gridBox);
     }
 }
