fd54a13e3f96a3630a3c55697798412bd09e1b0c
Hugo Perez <hugopereztm12309@gmail.com>
2021-08-10 17:30:00 -0400

    Created a new function to prompt the user and set some conditons they must follow. For example the grid can't be below 0 or greater then 100 otherwise the prompt will keep looping and asking them, as well as indented some code to make it easier to read.

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 const div = document.querySelector('#gridContainer');
 
-let gridSize = prompt("Please enter the grid size (e.g 2 : 2x2, 4 : 4x4, 25 : 25x25) : ");
-
-createGridBoxes(gridSize);
+createGridBoxes();
 changeGridBoxColor();
 onClickClearButton();
 
-function createGridBoxes(size) {
-    div.setAttribute('style', 'grid-template-columns: repeat('+size+', 1fr)');
-    for (let i = 0; i < size * size; i++) {
-        let gridBox = document.createElement('div');
-            gridBox.classList.add('gridBox');
-                div.appendChild(gridBox);
-    }
-}
+function promptUser() {
+    let looping = true;
+
+    while (looping) {
+        let gridSize = prompt("Please enter the grid size (e.g 2 : 2x2, 4 : 4x4, 25 : 25x25) : ");
+            if(gridSize == null || gridSize < 0) {
+                looping = true;
+            }
+            else if (gridSize >= 101) {
+                looping = true;
+            }
+            else if (gridSize != 0) {
+                return gridSize;
+            }
+            else {
+                return 16;
+            }
+    };
+};
+function createGridBoxes() {
+    let gridSize = promptUser();
+        div.setAttribute('style', 'grid-template-columns: repeat('+gridSize+', 1fr)');
+            for (let i = 0; i < gridSize * gridSize; i++) {
+                let gridBox = document.createElement('div');
+                    gridBox.classList.add('gridBox');
+                    div.appendChild(gridBox);
+    };
+};
 
 function changeGridBoxColor() {
     const gridBoxes = document.querySelectorAll(".gridBox");
@@ -22,16 +40,16 @@ function changeGridBoxColor() {
             gridBox.classList.add('active');
         });
     });
-}
+};
 function clearGridBoxColor() {
     const gridBoxes = document.querySelectorAll(".active");
-    gridBoxes.forEach((gridBox) => {
-        gridBox.classList.remove('active');
-    });
-}
+        gridBoxes.forEach((gridBox) => {
+            gridBox.classList.remove('active');
+        });
+};
 function onClickClearButton() {
     const clearButton = document.getElementById("clearButton");
-    clearButton.onclick = function() {
-            clearGridBoxColor();
+        clearButton.onclick = function() {
+                clearGridBoxColor();
         };
-}
+};
